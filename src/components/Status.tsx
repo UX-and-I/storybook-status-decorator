ec3654942eb587b370c6d82ce0094fe168db177d
Arne Kleinschmidt <arne.kleinschmidt@uxi.de>
2019-08-30 10:53:45 +0200

    adds jsx import

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
-import { css } from '@emotion/core';
+import { jsx, css } from '@emotion/core';
 
 const colorMap = {
   info: '#abcdef',
